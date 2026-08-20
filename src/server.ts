f5fe2308b526f9375b85d4a5e2b27f8916f67266
unknown <mdtanvir7462@gmail.com>
2026-08-20 21:34:50 +0600

    basic setup

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,17 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 const port = 5000;
 
+// middleware
+app.use(express.json()); //for parsing body data in json format
+app.use(express.urlencoded()); // for parsing body data in urlencoded format (form data)
+
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello 20th August!');
 });
-
+app.post("/", (req: Request, res: Response) => {
+   console.log(req.body);
+   res.send('POST request received!');
+});
 app.listen(port, () => {
    console.log(`http://localhost:${port}`);
 });
